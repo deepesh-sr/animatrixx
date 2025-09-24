6175573c073a3a300d9f2ab3b23365f24fd178a5
deepesh-sr <220714100078@cutm.ac.in>
2025-09-25 01:46:14 +0530

    agency page updated

--- src/components/HomeBottomText.tsx
+++ src/components/HomeBottomText.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className="flex font-[font2] flex-row justify-center gap-5 pb-2">
+      <div className="text-[5vw] border-4 hover:border-[#D3FD50] hover:text-[#D3FD50] text-white gap-2 px-7 pb-1 rounded-full"><Link to={'/projects'}>Projects</Link></div>
+      <div className="text-[5vw] border-4 hover:border-[#D3FD50] hover:text-[#D3FD50] text-white gap-2 px-7 pb-1 rounded-full"><Link to={'/agency'}>Agency</Link></div>
+    </div>
   )
 }
 
