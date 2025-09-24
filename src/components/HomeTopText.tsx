791a321d25f565b67b983de48f38622159f5afb9
deepesh-sr <220714100078@cutm.ac.in>
2025-09-24 23:32:55 +0530

    hometoptext style updated

--- src/components/HomeTopText.tsx
+++ src/components/HomeTopText.tsx
@@ -1,11 +1,13 @@
+import Video from "./home/Video"
+
 const HomeTopText = () => {
   return (
     <div className='font-[font1] uppercase text-[9vw] flex flex-col items-center leading-[8vw] text-white pt-4'>
         <div>
             The Buzz  
         </div>
-        <div>
-            Behind Every
+        <div className="flex flex-row justify-center justify-items-center">
+            Behind <div className="h-[6vw] w-[12vw] rounded-2xl overflow-hidden   "><Video/></div> Every
         </div>
         <div>
             Big Idea
