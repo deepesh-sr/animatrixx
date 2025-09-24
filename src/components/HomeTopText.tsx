d4a4771056d9978bce04ab6101339da9ed058e2b
deepesh-sr <220714100078@cutm.ac.in>
2025-09-24 20:24:58 +0530

    HometopText done :)

---
+++ src/components/HomeTopText.tsx
@@ -0,0 +1,17 @@
+const HomeTopText = () => {
+  return (
+    <div className='font-[font1] uppercase text-[9vw] flex flex-col items-center leading-[8vw] text-white pt-4'>
+        <div>
+            The Buzz  
+        </div>
+        <div>
+            Behind Every
+        </div>
+        <div>
+            Big Idea
+        </div>
+    </div>
+  )
+}
+
+export default HomeTopText
