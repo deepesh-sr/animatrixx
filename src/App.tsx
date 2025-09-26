884bb4111af4a297dc8d026cebcff870a1210ef5
deepesh-sr <220714100078@cutm.ac.in>
2025-09-26 12:04:57 +0530

    homepage transition animation added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,46 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Agency from "./pages/Agency"
 import Project from "./pages/Project"
+import gsap from 'gsap'
+import { useEffect, useRef } from "react"
 
 function App() {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.from('.stair', {
+      height: 0,
+      stagger: 0.1,
+    })
+
+    tl.to('.stair', {
+      y: '100%',
+      stagger: 0.1
+    })
+    
+    const timer = setTimeout(()=>{
+      divRef.current.style.display = 'none';
+      return clearInterval(timer)
+    },2000)
+    
+
+  }, [])
+
   return (
     <div>
+      <div ref={divRef} className="h-screen w-full flex flex-row fixed top-0 left-0 z-10">
+        <div className="stair bg-black h-screen w-1/5 "></div>
+        <div className="stair bg-black h-screen w-1/5 "></div>
+        <div className="stair bg-black h-screen w-1/5 "></div>
+        <div className="stair bg-black h-screen w-1/5 "></div>
+        <div className="stair bg-black h-screen w-1/5 "></div>
+      </div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/agency" element={<Agency/>}/>
-        <Route path="/project" element={<Project/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/agency" element={<Agency />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </div>
   )
--- src/pages/Agency.tsx
+++ src/pages/Agency.tsx
@@ -5,17 +5,45 @@ import { useEffect, useRef } from "react"
 
 const Agency = () => {
   gsap.registerPlugin(ScrollTrigger)
+  const imageDivRef = useRef(null);
   const imageRef = useRef(null);
 
+const imageArray = [
+'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg', 
+'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+'https://k72.ca/uploads/teamMembers/joe1_480X640_3-480x640.jpg'
+]
   useEffect(() => {
-    const element = imageRef.current
+    const element = imageDivRef.current
+    const image = imageRef.current
     gsap.to(element, {
       scrollTrigger: {
         trigger: element,
-        markers: true,
+        // markers: true,
         start: 'top 36%',
-        end: 'top -100%',
+        end: 'top -80%',
         pin: true,
+        onUpdate : (element)=>{
+          let imageIndex ;
+          if(element.progress < 14){
+            imageIndex = Math.floor(element.progress * imageArray.length)
+            image.src = imageArray[imageIndex]
+          }
+          else{
+            imageIndex = imageArray.length -1;
+          }
+        }
       },
       lazy: false
     });
@@ -29,8 +57,8 @@ const Agency = () => {
 
   return (
     <div>
-      <div ref={imageRef} className="w-[19vw] absolute left-[30%] top-[40%]">
-        <img className="w-full h-full rounded-3xl overflow-hidden" onLoad={()=>ScrollTrigger.refresh()} src=" https://k72.ca/images/teamMembers/Claire_640X960.jpg?w=640&h=960&s=8db7275995c2d79210fcf8641b5792fc" alt="image" />
+      <div ref={imageDivRef} className="w-[19vw] absolute left-[30%] top-[40%]">
+        <img ref={imageRef} className="w-full h-full rounded-3xl overflow-hidden" onLoad={()=>ScrollTrigger.refresh()} src=" https://k72.ca/images/teamMembers/Claire_640X960.jpg?w=640&h=960&s=8db7275995c2d79210fcf8641b5792fc" alt="image" />
       </div>
       <div className=" flex pl-4 absolute top-[45%] h-screen w-screen overflow-hidden">
         <h1 className='font-[font1] font-bold uppercase text-[18vw]'>
